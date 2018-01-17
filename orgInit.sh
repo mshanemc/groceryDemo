8ee934e3256d43da0d8f1aebcf3fa495ea61e4da
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-01-17 15:17:28 -0600

    users, chatter photos, task gamification

--- orgInit.sh
+++ orgInit.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
-
 echo 'Creating a scratch org'
 sfdx force:org:create -s -f config/project-scratch-def.json -d 1
 
 echo 'Pushing source to the scratch org'
 sfdx force:source:push
 
-# sfdx force:user:permset:assign -n TrialAdmin
-sfdx force:org:open
-
 sfdx force:data:tree:import -p data/pickupOrderPlan.json
 sfdx force:data:tree:import -f data/Task.json
 sfdx force:data:tree:import -f data/Event.json
+sfdx msm:user:password:set -g User -l User -p mobile123
 
+echo 'installing some packages in the background'
+sfdx force:package:install -i 04t1a000000VVbFAAW
+sfdx force:package:install -i 04t24000000kaar
+
+sfdx force:org:open
+
+sfdx force:user:create -f config/userDef/bandar.json
+sfdx force:user:create -f config/userDef/jodi.json
+sfdx force:user:create -f config/userDef/rahul.json
+
+sfdx msm:user:photo -f assets/bandar.jpeg -l El-Eita
+sfdx msm:user:photo -f assets/jodi.jpeg -l Innerfield
+sfdx msm:user:photo -f assets/rahul.jpeg -l Aswathy
+
+sfdx force:apex:execute -f scripts/setup.cls
+sfdx msm:user:photo -f assets/shane.jpeg -l McLaughlin
--- orgInitNonDefault.sh
+++ orgInitNonDefault.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'Creating a scratch org'
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -a grocerySpinoff
+sfdx msm:org:create -f config/project-scratch-def.json -d 1 -a grocerySpinoff -u shane -o grocery.demo
 
 echo 'Pushing source to the scratch org'
 sfdx force:source:push -u grocerySpinoff
@@ -8,6 +8,23 @@ sfdx force:source:push -u grocerySpinoff
 sfdx force:data:tree:import -p data/pickupOrderPlan.json -u grocerySpinoff
 sfdx force:data:tree:import -f data/Task.json -u grocerySpinoff
 sfdx force:data:tree:import -f data/Event.json -u grocerySpinoff
+sfdx msm:user:password:set -g User -l User -p mobile123 -u grocerySpinoff
 
-# sfdx force:user:permset:assign -n TrialAdmin
+echo 'installing some packages in the background'
+# package for task components
+sfdx force:package:install -i 04t1a000000VVbFAAW -u grocerySpinoff
+# package for e-signature
+sfdx force:package:install -i 04t24000000kaar -u grocerySpinoff
+
 sfdx force:org:open -u grocerySpinoff
+
+sfdx force:user:create -f config/userDef/bandar.json -u grocerySpinoff
+sfdx force:user:create -f config/userDef/jodi.json -u grocerySpinoff
+sfdx force:user:create -f config/userDef/rahul.json -u grocerySpinoff
+
+sfdx msm:user:photo -f assets/bandar.jpeg -l El-Eita -u grocerySpinoff
+sfdx msm:user:photo -f assets/jodi.jpeg -l Innerfield -u grocerySpinoff
+sfdx msm:user:photo -f assets/rahul.jpeg -l Aswathy -u grocerySpinoff
+
+sfdx force:apex:execute -f scripts/setup.cls -u grocerySpinoff
+sfdx msm:user:photo -f assets/shane.jpeg -l McLaughlin -u grocerySpinoff
